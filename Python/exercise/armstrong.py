77a3047943034eca013254303074d11d3e2acf58
varsha <varshachinna12@gmail.com>
2023-09-07 11:03:28 +0530

    exercise

--- Python/exercise/armstrong.py
+++ Python/exercise/armstrong.py
@@ -1,14 +1,15 @@
 n=int(input("enter the number\n"))
 r=0
 s=0
+x=n
 for i in range(n):
     r=n%10
     n=n//10
-    s=s+(r*r*r)
+    s=s+(r**3)
 print(s)
-if(n==s):
-    print("the num is armstrong num\n")
+if x==s:
+ print("the num is armstrong num\n")
 else:
-    print("the num is not armstrong num\n")
+ print("the num is not armstrong num\n")
 
 
